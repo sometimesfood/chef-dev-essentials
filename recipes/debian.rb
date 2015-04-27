7e55f14c181edd89b00de0013b38c8257f9a2f76
Sebastian Boehm <sebastian@sometimesfood.org>
2015-04-27 09:31:20 +0200

    Fix typo in debian package list

--- recipes/debian.rb
+++ recipes/debian.rb
@@ -10,7 +10,7 @@ debian_packages = ['devscripts',
                    'libio-socket-ssl-perl', # useful for uscan and GitHub
                    'pristine-tar',
                    'git-buildpackage',
-                   'debian-el'
+                   'debian-el',
                    'quilt']
 
 if node['platform'] == 'ubuntu'
