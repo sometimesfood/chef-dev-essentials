15ae05af64a2eebbd74b6c02193419d3119d7685
Sebastian Boehm <sebastian@sometimesfood.org>
2015-07-09 14:28:11 +0200

    Install valgrind

--- recipes/cpp.rb
+++ recipes/cpp.rb
@@ -1,4 +1,5 @@
+DevEssentials.dont_clone_resource(self, :package, 'build-essential')
 package 'cmake'
 package 'g++'
 package 'clang'
-DevEssentials.dont_clone_resource(self, :package, 'build-essential')
+package 'valgrind'
